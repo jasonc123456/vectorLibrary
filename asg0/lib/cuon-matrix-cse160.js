ce7a17a949b5564f607754b9b0b7ec63cf46815a
Jason Cheung <jasonc123456@corecrafted.net>
2026-01-08 06:55:06 +0000

    added add sub mul and div operation to the lib js

--- asg0/lib/cuon-matrix-cse160.js
+++ asg0/lib/cuon-matrix-cse160.js
@@ -46,8 +46,9 @@ class Vector3 {
     add(other) {
         // Insert your code here.
         // This function should change this vector (this.elements) and not create a new vector.
-
-        // Don't delete the return statement.
+        this.elements[0] += other.elements[0];
+        this.elements[1] += other.elements[1];
+        this.elements[2] += other.elements[2];
         return this;
     };
 
@@ -58,7 +59,9 @@ class Vector3 {
     sub(other) {
         // Insert your code here.
         // This function should change this vector (this.elements) and not create a new vector.
-
+        this.elements[0] -= other.elements[0];
+        this.elements[1] -= other.elements[1];
+        this.elements[2] -= other.elements[2];
         // Don't delete the return statement.
         return this;
     };
@@ -70,7 +73,9 @@ class Vector3 {
     div(scalar) {
         // Insert your code here.
         // This function should change this vector (this.elements) and not create a new vector.
-
+        this.elements[0] /= other.elements[0];
+        this.elements[1] /= other.elements[1];
+        this.elements[2] /= other.elements[2];
         // Don't delete the return statement.
         return this;
     };
@@ -82,7 +87,9 @@ class Vector3 {
     mul(scalar) {
         // Insert your code here.
         // This function should change this vector (this.elements) and not create a new vector.
-
+        this.elements[0] *= other.elements[0];
+        this.elements[1] *= other.elements[1];
+        this.elements[2] *= other.elements[2];
         // Don't delete the return statement.
         return this;
     };
